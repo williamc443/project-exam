20e875f1b48ef3dddf503647f670f946f4ba144a
William <williamc443@hotmail.com>
2021-02-03 22:26:23 +0100

    Update countdown.js
    Added the countdown timer, Set interval function, timeleft.

--- js/countdown.js
+++ js/countdown.js
@@ -9,5 +9,26 @@
      })
      .catch(function (error) {
          console.log(error);
-         
      });
+
+ function countdownTimer(nextLaunch) {
+     const nextLaunchTime = nextLaunch.date_utc;
+     const countdownDate = new Date(nextLaunchTime).getTime();
+     document.querySelector(".countdown");
+
+     setInterval(function () {
+         const timeNow = new Date().getTime();
+         const timeleft = countdownDate - timeNow;
+
+         // Days, hours, minutes and seconds left
+         let days = Math.floor(timeleft / (1000 * 60 * 60 * 24));
+         let hours = Math.floor((timeleft % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+         let minutes = Math.floor((timeleft % (1000 * 60 * 60)) / (1000 * 60));
+         let seconds = Math.floor((timeleft % (1000 * 60)) / 1000);
+
+         
+
+
+     }, 1000)
+
+    }
