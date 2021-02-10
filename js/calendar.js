dba6d2436130f1a62fe2ae913ca29e4e458e4ec9
William <williamc443@hotmail.com>
2021-02-10 23:21:13 +0100

    Adding the first days of the next month

--- js/calendar.js
+++ js/calendar.js
@@ -23,6 +23,14 @@ console.log(prevLastDay);
 
 const firstDayIndex = date.getDay();
 
+const lastDayIndex = new Date(date.getFullYear(),
+date.getMonth() + 1, 0).getDay();
+
+// Checking the last day of the month=
+// console.log(lastDayIndex);
+
+const nextDays = 7 - lastDayIndex - 1;
+
 const months = [
     "january",
     "February",
@@ -45,16 +53,22 @@ document.querySelector('.date p').innerHTML= date.toDateString();
 let days = "";
 
 for(let x = firstDayIndex; x > 0; x--) {
-    days += `<div class="prev-date">${prevLastDay - x}</div>`;
+    days += `<div class="prev-date">${prevLastDay - x + 1}</div>`;
 }
 
 for(let i = 1; i <= lastDay; i++) {
     days += `<div>${i}</div>`;
-    monthDays.innerHTML = days;
+}
+    // monthDays.innerHTML = days;
     // x += 10  x = x + 10
-} 
+
 
 // const month = date.getMonth();
 
 // console.log(month);
+
+for(let j = 1; J <= nextDays; j++) {
+    days += `<div class="next-date">${j}</div>`;
+    monthDays.innerHTML = days;
+}
 
